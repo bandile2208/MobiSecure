7343f46cfe81c541b9fa73987ab6fa5a67826886
bandile2208 <gazubandile03@gmail.com>
2024-08-15 21:28:51 +0200

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Button } from 'react-native';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
+
+function SignOutButton() {
+  const { signOut } = useAuthenticator();
+  return <Button title="Sign Out" onPress={signOut} />;
+}
+
+function App() {
+  return (
+    <Authenticator.Provider>
+      <Authenticator loginMechanisms={['email']}>
+        <SignOutButton />
+      </Authenticator>
+    </Authenticator.Provider>
+  );
+}
+
+export default App;
