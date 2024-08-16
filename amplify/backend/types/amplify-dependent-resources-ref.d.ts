52b34c0fcbbcb5128b30d43bfd4922ede3c8a333
bandile2208 <gazubandile03@gmail.com>
2024-08-16 02:01:59 +0200

    Authentication Working Completely

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,22 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mobisecure26b6a178": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "mobisecure26b6a178CustomMessage": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,40 @@
-import { Button } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import { Amplify } from 'aws-amplify';
 import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
+
+import awsconfig from '../src/aws-exports';
+Amplify.configure(awsconfig);
+
+const MySignInFooter = () => <Text>My Footer</Text>;
 
 function SignOutButton() {
   const { signOut } = useAuthenticator();
-  return <Button title="Sign Out" onPress={signOut} />;
+  return <Button onPress={signOut} title="Sign Out" />;
 }
 
 function App() {
   return (
     <Authenticator.Provider>
-      <Authenticator loginMechanisms={['email']}>
-        <SignOutButton />
+      <Authenticator
+        components={{
+          SignUp: (props) => (
+            // will render only on the SignIn subcomponent
+            <Authenticator.SignUp {...props} Footer={MySignInFooter} />
+          ),
+        }}
+      >
+        <View style={style.container}>
+          <SignOutButton />
+        </View>
       </Authenticator>
     </Authenticator.Provider>
   );
 }
+
+const style = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+});
 
 export default App;
